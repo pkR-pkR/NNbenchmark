7eedbc46e96f70723dfd7fd3152adfd81d564c27
kiener <patrice.kiener@inmodelia.com>
2019-08-13 22:21:57 +0200

    NNbenchmark_2.2 => NNdataSummary()

---
+++ R/NNdataSummary.R
@@ -0,0 +1,28 @@
+## NNdataSummary
+
+
+#' @title A Summary (in data.frame format) of NNdatasets
+#' @description
+#' \code{NNdataSummary} summarizes the information of the 12 datasets
+#' listed in \code{NNdatasets}: name, n_rows, n_inputs, n_neurons, 
+#' n_parameters. 
+#' 
+#' @param   NNdatasets   the NNdatasets list.
+#' @examples 
+#' NNdataSummary(NNdatasets)
+#' 
+#' @export
+#' @name NNdataSummary
+NNdataSummary <- function(NNdatasets) {
+    data.frame(
+        Dataset   = names(NNdatasets),
+        n_rows    = sapply(NNdatasets, function(lst) nrow(lst[["Z"]])),
+        n_inputs  = sapply(NNdatasets, function(lst) ncol(lst[["Z"]])-1),
+        n_neurons = sapply(NNdatasets, function(lst) lst[["neur"]]),
+        n_parameters = sapply(NNdatasets, function(lst) lst[["nparNN"]]),
+        row.names = NULL
+    )
+}
+
+
+
