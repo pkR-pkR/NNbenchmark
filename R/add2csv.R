73cbdd406663d71f704339409e4872c956640939
kiener <patrice.kiener@inmodelia.com>
2019-08-20 14:01:41 +0200

    NNbenchmark_2.3.1 add2csv tempdir() + datalist

--- R/add2csv.R
+++ R/add2csv.R
@@ -1,7 +1,7 @@
 ## add2csv 2019-08-04
 
 
-#' @title Create or Append a data.frame To a csv File
+#' @title Create or Append a data.frame to a csv File
 #' @description
 #' Create or append a data.frame to a csv file. Column names are added at creation and 
 #' ignored at the append steps.
@@ -10,11 +10,12 @@
 #' @param  file   character. The filename.
 #' 
 #' @examples 
-#' x <- data.frame(a = 1:4, b = 5:8)  
-#' add2csv(x) 
-#' add2csv(x) 
-#' add2csv(x)
-#' read.csv("results.csv")
+#' results_csv <- tempfile("results", fileext = ".csv")
+#' x <- data.frame(a = 1:3, b = 4:6) 
+#' add2csv(x, file = results_csv) 
+#' add2csv(x*10, file = results_csv) 
+#' add2csv(x*100, file = results_csv) 
+#' read.csv(file = results_csv) 
 #' 
 #' @export
 #' @name add2csv
