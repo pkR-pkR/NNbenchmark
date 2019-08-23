04a7d557f94bd8709bba0a09088d29dd3f561875
kiener <patrice.kiener@inmodelia.com>
2019-08-23 15:54:25 +0200

    NNbenchmark_2.3.5 + Remove Try-New-Dir

--- R/a_intro.R
+++ R/a_intro.R
@@ -1,4 +1,4 @@
-## a_intro = NNbenchmark-package 2019-08-04
+## a_intro = NNbenchmark-package 2019-08-04 + 2019-08-22
 
 
 
@@ -6,6 +6,9 @@
 #' @description
 #' Datasets and functions to benchmark (convergence, speed, ease of use) R packages 
 #' dedicated to regression with neural networks (no classification in this version).
+#' The templates for the tested packages are available at 
+#' <https://github.com/pkR-pkR/NNbenchmarkTemplates/tree/master/R> and 
+#' <https://akshajverma.com/NNbenchmarkWeb>.
 #' @examples
 #' ds <- grep("^[m,u]", ls("package:NNbenchmark"), value = TRUE); ds
 #' t(sapply(ds, function(x) dim(get(x)))) 
